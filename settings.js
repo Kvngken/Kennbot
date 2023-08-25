f1acc7d9b448b10fda46f082cc8ea01f21746de6
Kvngken <139264692+Kvngken@users.noreply.github.com>
2023-08-25 01:34:42 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,18 +21,18 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['916909137213'] //ur owner number
-global.ownernomer = "916909137213" //ur owner number2
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.owner = ['2348033493003'] //ur owner number
+global.ownernomer = "2348033493003" //ur owner number2
+global.ownername = "Kendrick🖤" //ur owner name
+global.ytname = "YT: folexkendrick" //ur yt chanel name
+global.socialm = "GitHub: Kvngken" //ur github or insta name
+global.location = "Nigeria, Lagos, Lekki" //ur location
 
 //new
-global.botname = "Cheems Bot MD V8"
-global.ownernumber = '916909137213'
-global.ownername = '🦄드림 가이 Xeon'
-global.ownerNumber = ["916909137213@s.whatsapp.net"]
+global.botname = "KvngKenBot"
+global.ownernumber = '2348033493003'
+global.ownername = 'Kendrick🖤'
+global.ownerNumber = ["2348033492003@s.whatsapp.net"]
 global.ownerweb = "https://youtube.com/@DGXeon"
 global.websitex = "https://youtu.be/L_SIk59QeAU"
 global.wagc = "https://chat.whatsapp.com/Dc2qyVeK8JbJq8Gr3U1pKH"
